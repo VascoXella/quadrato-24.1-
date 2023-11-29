b28ee8815c49a992c123dd6d88eb779cbb8b1a2f
Utilizzatore <Utilizzatore@DESKTOP-4DDKKF3>
2023-11-29 21:18:17 +0100

    Aggiungere i file di progetto.

---
+++ quadrato (24.1)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quadrato__24._1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrato q = new Quadrato();
+
+            Console.WriteLine("inserisci il lato:");
+            q.Lato = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("l'area è: {0}", q.Lato * q.Lato);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ quadrato (24.1)/Quadrato.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quadrato__24._1_
+{
+    internal class Quadrato
+    {
+        public float Lato;
+    }
+}
